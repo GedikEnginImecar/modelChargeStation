9b6eae6b6a5eab9bec433d6ffc32d08eff9e3c87
Engin Gedik <enggdk13@gmail.com>
2024-05-14 18:17:18 +0300

    tried different i2s approaches to keep flash usage low researched alternative options and noted them down

--- code/mcu/esp32wroom/spiConnection/speaker/archive/pinTestSPI.cpp
+++ code/mcu/esp32wroom/spiConnection/speaker/archive/pinTestSPI.cpp
@@ -20,3 +20,11 @@ void loop()
 {
     // put your main code here, to run repeatedly:
 }
+
+// https://wokwi.com/projects/330669951756010068 example to look at
+// EspSoftwareSerial by Dirk Kaar
+// ESP32-audioI2S by schreibfaul1
+// https: // github.com/pschatzmann/ESP32-A2DP/wiki
+
+// look at how to send data, bit clock, lrc etc from esp32 i2s to either speakers or the dac
+// https://www.youtube.com/watch?v=qNLvoSQCx60

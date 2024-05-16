8e0727594d2bd71a138c5366f69f90d9b393b93c
Engin Gedik <enggdk13@gmail.com>
2024-05-16 09:16:35 +0300

    organized the setup file

--- code/mcu/finalBuilds/src/main.cpp
+++ code/mcu/finalBuilds/src/main.cpp
@@ -13,45 +13,50 @@
 I2SStream i2s;
 BluetoothA2DPSink a2dp_sink(i2s);
 
+void audioStreamingSetup()
+{
+  // regular config for the audio transmission - more suitable for external DAC/ADC that does not require software tuning
+  auto cfg = i2s.defaultConfig();
+  cfg.pin_bck = 16;  // Bit clock
+  cfg.pin_ws = 5;    // Stereo pin
+  cfg.pin_data = 17; // Data transfer pin (esp to dac)
+  i2s.begin(cfg);    // filling in the class variable values and starting the stream
+
+  a2dp_sink.start("MyMusic"); // creating bluetooth instantiation
+};
+
+void ledSetup()
+{
+  // rgb pin outputs being defined
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+};
+
 // first func to call
 void setup()
 {
   // high baud rate to allow for debugging and faster communication
   Serial.begin(115200);
 
-  // regular config for the audio transmission - more suitable for external DAC/ADC that does not require software tuning
-  auto cfg = i2s.defaultConfig();
-  cfg.pin_bck = 16;  // Bitclock
-  cfg.pin_ws = 5;    // Stereo pin
-  cfg.pin_data = 17; // Data transfer pin (esp to dac)
-  i2s.begin(cfg);    // filling in the class variable values and starting the stream
-
-  a2dp_sink.start("MyMusic"); // creating bluetooth instantiation
-
-  // rgb pin outputs being defined
-  pinMode(redPin, OUTPUT);
-  pinMode(greenPin, OUTPUT);
-  pinMode(bluePin, OUTPUT);
+  // calls other setup functions
+  audioStreamingSetup();
+  ledSetup();
 }
 
 void loop()
 {
-  // basic cycling - should either use multithreading to not interrupt audio transmission or use millis to create a timer loop as opposed to delaying the entire program
-  delay(500);
-  Serial.println("red");
-  analogWrite(redPin, 255);
-  analogWrite(greenPin, 0);
-  analogWrite(bluePin, 0);
+  // basic led effect - should either use multithreading to not interrupt audio transmission or use millis to create a timer loop as opposed to delaying the entire program
 
   delay(500);
-  Serial.println("green");
-  analogWrite(redPin, 0);
-  analogWrite(greenPin, 255);
-  analogWrite(bluePin, 0);
-
-  delay(500);
-  Serial.println("blue");
+  Serial.println("blue high");
   analogWrite(redPin, 0);
   analogWrite(greenPin, 0);
   analogWrite(bluePin, 255);
+
+  delay(500);
+  Serial.println("blue low");
+  analogWrite(redPin, 0);
+  analogWrite(greenPin, 0);
+  analogWrite(bluePin, 50);
 }

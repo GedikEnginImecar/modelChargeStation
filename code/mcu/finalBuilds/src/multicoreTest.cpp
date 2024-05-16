45b5402c260cdd9c13ad3715e58fbcceb090a948
Engin Gedik <enggdk13@gmail.com>
2024-05-16 17:54:03 +0300

    creating initial tasks to run concurrently

--- code/mcu/finalBuilds/src/multicoreTest.cpp
+++ code/mcu/finalBuilds/src/multicoreTest.cpp
@@ -1,2 +1,50 @@
 // multicoreTest.cpp
 // script to test and reaffirm prior understanding and knowledge of utilizing multi core processing on esp32
+
+#include "Arduino.h"
+#include "AudioTools.h"
+#include "BluetoothA2DPSink.h"
+#include "BluetoothA2DPSource.h"
+
+int count1 = 0;
+int count2 = 0;
+
+// pointer to void parameters
+// used when creating a FreeRTOS task
+// allows you to pass parameters of any kind into the task function
+// allows for flexibility and transfer of data between tasks
+// pvParameters "pv" stands for pointer void, directly shows that it is a void pointer
+void task1(void *pvParameters)
+{
+    for (;;) // infinite for loop
+    {
+        Serial.print("Task 1 counter:");
+        Serial.println(count1++);              // increments counter
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // vTaskDelay counts in ticks, but 1000/portTICK_PERIOD_MS converts it into ms by using constant built in
+    }
+};
+
+void task2(void *pvParameters)
+{
+    for (;;) // infinite for loop
+    {
+        Serial.print("Task 2 counter:");
+        Serial.println(count2++);              // increments counter
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // vTaskDelay counts in ticks, but 1000/portTICK_PERIOD_MS converts it into ms by using constant built in
+    }
+};
+
+void setup()
+{
+    Serial.begin(9600);
+    // xTaskCreate(function to run,
+    // name/description of the task - debug purposes and you can use FreeRTOS to display info about running tasks
+    // stack size - esp32 uses bytes but normally expressed in words. value depends on function, it increases with more variables used, can require more if connecting to wifi or loading heavier libs
+    // the task parameter to receive when the task is created, if nothing it can be null
+    // task priority, the higher the number the higher priority the task will be, FreeRTOS will give that task more time
+    // used to get a task handle, allowing tasks to interact with other tasks);
+    xTaskCreate(task1, "Task 1", 1000, NULL, 1, NULL);
+    xTaskCreate(task2, "Task 2", 1000, NULL, 1, NULL);
+}
+
+void loop() {}
